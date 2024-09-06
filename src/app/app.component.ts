b21e87d8f367968f2c2af142f89b5edf3d532258
Donattah <d.ajuang@carepay.com>
2024-09-07 00:43:59 +0300

    add-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+
+interface RegisterForm  {
+  username: FormControl<string | null>;
+  email: FormControl<string | null>;
+  password:FormControl<string | null>;
+  gender:FormControl<string | null>
+
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-forms';
+  title = 'Angular reactive form';
+
+  registerForm: FormGroup<RegisterForm> = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+  });
+
+  genders = [
+    {id:1, name:'male'},
+    {id:2, name:'female'},
+    {id:2, name:'other'}
+  ]
+
+onRegister(): void {
+  this.registerForm.markAllAsTouched();
+  this.registerForm.dirty;
+  console.log("registerForm", this.registerForm.value);
+  console.log("registerForm", this.registerForm.getRawValue());
+
+}
+  
+
 }
